ac36f20ecc23b1602b6bd6244b9cdc876ff2c04e
SergenUCLL <sergen.schoufs@student.ucll.be>
2024-12-20 11:01:02 +0100

    fixxed create operation

--- back-end/service/appointment.service.ts
+++ back-end/service/appointment.service.ts
@@ -2,15 +2,26 @@ import { Appointment } from '../model/appointment';
 import appointmentDb from '../repository/appointment.db';
 import { AppointmentInput } from '../types';
 
-// const createAppointment = async (appointmentInput: AppointmentInput): Promise<Appointment> => {
-
-// };
+const createAppointment = async (appointmentInput: AppointmentInput): Promise<Appointment> => {
+    try {
+        const newAppointment = new Appointment({
+            title: appointmentInput.title,
+            startDate: appointmentInput.startDate,
+            endDate: appointmentInput.endDate,
+            note: appointmentInput.note,
+        });
+        return await appointmentDb.createAppointment(newAppointment);
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to create appointment');
+    }
+};
 
 const getAllAppointments = async (): Promise<Appointment[]> => {
     return await appointmentDb.getAllAppointments();
 };
 
 export default {
-    // createAppointment,
+    createAppointment,
     getAllAppointments,
 };
